f3aea6802bae5a934aa7d20be2423b73f2f99c68
albert <albert@albert-XPS-L501X.(none)>
2014-05-10 09:29:50 -0400

    Added traversal method

--- BST.rb
+++ BST.rb
@@ -4,11 +4,11 @@ class BSTNode
 
     attr_accessor :left, :right, :item
 
-    def initialize(item)
+    def initialize item
         @item = item
     end
 
-    def <=>(other)
+    def <=> other
         return @item <=> other.item
     end
 end
@@ -28,26 +28,50 @@ class BST
 
     ##
     # Adds the item to the bst
-    def add(item)
+    def add item
         @root = addHelper item, @root
     end
 
     ##
     # Returns true if the item has been added to the tree, false otherwise. Checks using the == operator
-    def contains(item)
+    def contains item
         return containsHelper item, @root
     end
 
     ##
     # Removes the value from the tree.  If applicable, replaces with the successor
-    def remove(item)
+    def remove item
         @root = removeHelper item, @root
     end 
+
+    ##
+    # Returns a preorder traversal of the tree
+    def preorder
+        ret = Array.new
+        preorderHelper @root, ret
+        return ret
+    end
+
+    ##
+    # Returns a preorder traversal of the tree
+    def inorder
+        ret = Array.new
+        inorderHelper @root, ret
+        return ret
+    end
+
+    ##
+    # Returns a postorder traversal of the tree
+    def postorder
+        ret = Array.new
+        postorderHelper @root, ret
+        return ret
+    end
 
     private 
 
     # Recurses through the bst for adding
-    def addHelper(item, root)
+    def addHelper item, root
         if !root
             @size = @size + 1
             return BSTNode.new item
@@ -65,7 +89,7 @@ class BST
     end
 
     # Recurses through the bst for contains
-    def containsHelper(item, root)
+    def containsHelper item, root
         if !root
             return false
         else
@@ -80,7 +104,7 @@ class BST
     end
 
     # Recurses through the tree for remove
-    def removeHelper(item, root)
+    def removeHelper item, root
         if !root
             return nil
         else
@@ -114,13 +138,39 @@ class BST
         end
     end
 
-    def getLargest(root)
-        if(!root.right)
+    #Gets the largest item in the subtree
+    def getLargest root
+        if !root.right
             return root.item
         else
             return getLargest root.right
         end
     end
+
+    def preorderHelper root, arr
+        if root
+            arr.push root.item
+            preorderHelper root.left, arr
+            preorderHelper root.right, arr
+        end
+    end
+
+    def inorderHelper root, arr
+        if root
+            preorderHelper root.left, arr
+            arr.push root.item
+            preorderHelper root.right, arr
+        end
+    end
+
+    def postorderHelper root, arr
+        if root
+            preorderHelper root.left, arr
+            preorderHelper root.right, arr
+            arr.push root.item
+        end
+    end
+
 end
 
 bst = BST.new
@@ -131,10 +181,11 @@ bst.add 3
 bst.add 7
 bst.add 12
 bst.add 20
-puts bst.size
-bst.remove 10
-puts bst.size
-puts bst.root.item
+# puts bst.size
+# bst.remove 10
+# puts bst.size
+# puts bst.root.item
+puts bst.preorder
 # puts bst.contains 10
 # puts bst.contains 5
 # puts bst.contains 15
